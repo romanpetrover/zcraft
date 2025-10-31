d9ab0225cdeca9edc895b5da113f3f62edb62798
romanpetrover <jzdogr6651@outlook.com>
2025-10-31 21:57:22 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedGridSelection = await deploy("ZCraft", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ZCraft contract: `, deployedGridSelection.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_ZCraft"; // id required to prevent reexecution
+func.tags = ["ZCraft"];
